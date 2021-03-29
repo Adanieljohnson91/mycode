5b91b1bbf8ad7e2871934fff8b350538ac4e0a8c
Anthony Johnson <dan.johnson1022@gmail.com>
2021-03-29 20:19:24 +0000

    learning about dictionaries

--- dict01/pythondict01.py
+++ dict01/pythondict01.py
@@ -8,7 +8,41 @@ print( switch["hostname"] )
 print( switch["ip"] )
 
 ## request a 'fake' key
-print( switch["lynx"] )  
+##print( switch["lynx"] )  
 tup = (1,"a",[], {}, "five")
 for i in tup:
-    print(i.class)
+    print(type(i))
+
+
+print( "First test - .get()" )
+print( switch.get("lynx") )
+
+print( "Second test - .get()" )
+print( switch.get("lynx", "THE KEY IS IN ANOTHER CASTLE!") )
+
+print( "Third test - .get()" )
+print( switch.get("version") )
+
+
+print( "Fourth test - .keys()" )
+print( switch.keys() )
+
+print( "Fifth test - .values()" )
+print( switch.values() )
+
+
+print( "Sixth test - .pop()" )
+switch.pop("version") # removes this key (and value) pair
+print( switch.keys() )   # notice the value of version is gone
+print( switch.values() ) # notice the value 1.2
+
+print( "Seventh test - ADD a new value" )
+switch["adminlogin"] = "karl08"
+print( switch.keys() )
+print( switch.values() )
+
+print( "Eighth test - ADD a new value" )
+switch["password"] = "qwerty"
+print( switch.keys() )
+print( switch.values() )
+
